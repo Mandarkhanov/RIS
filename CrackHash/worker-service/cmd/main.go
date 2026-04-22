c8aaaea09ee4d6b1c7d81746583cf6fb7786815c
Danil Mandarkhanov <danil.man4study@gmail.com>
2026-04-23 02:22:21 +0700

    refactor worker

--- CrackHash/worker-service/cmd/main.go
+++ CrackHash/worker-service/cmd/main.go
@@ -2,55 +2,47 @@ package main
 
 import (
 	"context"
+	"crackhash/worker/internal/config"
+	"crackhash/worker/internal/service"
+	"crackhash/worker/internal/transport/rest"
 	"log"
 	"net/http"
 	"sync"
 )
 
 type WorkerApp struct {
-	cfg         *Config
+	cfg         *config.Config
 	activeTasks map[string]context.CancelFunc
 	mu          sync.Mutex
 }
 
-func NewWorkerApp(cfg *Config) *WorkerApp {
+func NewWorkerApp(cfg *config.Config) *WorkerApp {
 	return &WorkerApp{
 		cfg:         cfg,
 		activeTasks: make(map[string]context.CancelFunc),
 	}
 }
 
-const (
-	WorkerTaskPath   = "/internal/api/worker/hash/crack/task"
-	WorkerCancelPath = "/internal/api/worker/hash/crack/cancel"
-
-	ManagerRequestPath = "/internal/api/manager/hash/crack/request"
-)
-
-func (app *WorkerApp) setupServer() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc(WorkerTaskPath, app.handleTask)
-	mux.HandleFunc(WorkerCancelPath, app.handleCancel)
-	return mux
-}
-
 func main() {
-	cfg, err := LoadConfig()
+	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("Failed to load config : %v", err)
 	}
 
-	app := NewWorkerApp(cfg)
-	mux := app.setupServer()
+	crackWorkerService := service.NewCrackWorkerService(cfg)
+
+	handler := rest.NewHandler(crackWorkerService)
+	mux := handler.InitRoutes()
+
 	srv := &http.Server{
-		Addr:         ":" + app.cfg.Port,
+		Addr:         ":" + cfg.Port,
 		Handler:      mux,
-		ReadTimeout:  app.cfg.ReadTimeout,
-		WriteTimeout: app.cfg.WriteTimeout,
-		IdleTimeout:  app.cfg.IdleTimeout,
+		ReadTimeout:  cfg.ReadTimeout,
+		WriteTimeout: cfg.WriteTimeout,
+		IdleTimeout:  cfg.IdleTimeout,
 	}
 
-	log.Printf("Worker started on :%s", app.cfg.Port)
+	log.Printf("Worker started on :%s", cfg.Port)
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatalf("Server error: %v", err)
 	}
