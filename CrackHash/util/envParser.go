984b55fa65976d93122951fa85716d134076f9a9
Danil Mandarkhanov <danil.man4study@gmail.com>
2026-04-22 19:05:52 +0700

    refactor envParser

--- CrackHash/util/envParser.go
+++ CrackHash/util/envParser.go
@@ -1,33 +1,40 @@
 package util
 
 import (
+	"log"
 	"os"
 	"strconv"
 	"time"
 )
 
 func ParseString(envKey string, defaultVal string) string {
-	valStr := os.Getenv(envKey)
-	if valStr == "" {
+	valStr, exists := os.LookupEnv(envKey)
+	if !exists {
+		log.Printf("Environment variable \"%s\" not set. Using default value: %s\n", envKey, defaultVal)
 		return defaultVal
 	}
 	return valStr
 }
 
 func ParseDuration(envKey string, defaultVal time.Duration) time.Duration {
-	valStr := os.Getenv(envKey)
-	if valStr == "" {
+	valStr, exists := os.LookupEnv(envKey)
+	if !exists {
+		log.Printf("Environment variable \"%s\" not set. Using default value: %s\n", envKey, defaultVal)
 		return defaultVal
 	}
-	if valInt, err := strconv.Atoi(valStr); err == nil {
-		return time.Duration(valInt) * time.Second
+
+	duration, err := time.ParseDuration(valStr)
+	if err != nil {
+		log.Printf("Invalid format for environment variable \"%s\": \"%s\". Using default value: %s. Error: %v\n", envKey, valStr, defaultVal, err)
+		return defaultVal
 	}
-	return defaultVal
+	return duration
 }
 
 func ParseInt(envKey string, defaultVal int) int {
-	valStr := os.Getenv(envKey)
-	if valStr == "" {
+	valStr, exists := os.LookupEnv(envKey)
+	if !exists {
+		log.Printf("Environment variable \"%s\" not set. Using default value: %s\n", envKey, int(defaultVal))
 		return defaultVal
 	}
 	if valInt, err := strconv.ParseInt(valStr, 10, 64); err == nil && valInt > 0 {
@@ -37,8 +44,9 @@ func ParseInt(envKey string, defaultVal int) int {
 }
 
 func ParseBool(envKey string, defaultVal bool) bool {
-	valStr := os.Getenv(envKey)
-	if valStr == "" {
+	valStr, exists := os.LookupEnv(envKey)
+	if !exists {
+		log.Printf("Environment variable \"%s\" not set. Using default value: %s\n", envKey, bool(defaultVal))
 		return defaultVal
 	}
 	if valBool, err := strconv.ParseBool(valStr); err == nil {
