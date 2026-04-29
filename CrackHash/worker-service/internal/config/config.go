24801f38a223739f8dcd422f38560a9ae57ac7da
Danil Mandarkhanov <danil.man4study@gmail.com>
2026-04-30 00:07:35 +0700

    add rabbitmq service in docker-compose & update configs

--- CrackHash/worker-service/internal/config/config.go
+++ CrackHash/worker-service/internal/config/config.go
@@ -7,35 +7,39 @@ import (
 
 const (
 	DefaultPort                   = "8081"
-	DefaultManagerURL             = "http://localhost:8080"
 	DefaultReadTimeout            = 10 * time.Second
 	DefaultWriteTimeout           = 10 * time.Second
 	DefaultIdleTimeout            = 120 * time.Second
 	DefaultClientTimeout          = 10 * time.Second
 	DefaultContextCheckIterations = 10000
 	DefaultStopOnFirstMatch       = true
+	DefaultRabbitMQURL            = "amqp://guest:guest@localhost:5672/"
+	DefaultTasksQueue             = "tasks_queue"
+	DefaultResultsQueue           = "results_queue"
 )
 
 type Config struct {
 	Port                   string
-	ManagerURL             string
 	ReadTimeout            time.Duration
 	WriteTimeout           time.Duration
 	IdleTimeout            time.Duration
-	ClientTimeout          time.Duration
 	ContextCheckIterations int
 	StopOnFirstMatch       bool
+	RabbitMQURL            string
+	TasksQueue             string
+	ResultsQueue           string
 }
 
 func LoadConfig() (*Config, error) {
 	return &Config{
 		Port:                   util.ParseString("PORT", DefaultPort),
-		ManagerURL:             util.ParseString("MANAGER_URL", DefaultManagerURL),
 		ReadTimeout:            util.ParseDuration("READ_TIMEOUT", DefaultReadTimeout),
 		WriteTimeout:           util.ParseDuration("WRITE_TIMEOUT", DefaultWriteTimeout),
 		IdleTimeout:            util.ParseDuration("IDLE_TIMEOUT", DefaultIdleTimeout),
-		ClientTimeout:          util.ParseDuration("CLIENT_TIMEOUT", DefaultClientTimeout),
 		ContextCheckIterations: util.ParseInt("CONTEXT_CHECK_ITERATIONS", DefaultContextCheckIterations),
 		StopOnFirstMatch:       util.ParseBool("STOP_ON_FIRST_MATCH", DefaultStopOnFirstMatch),
+		RabbitMQURL:            util.ParseString("RABBITMQ_URL", DefaultRabbitMQURL),
+		TasksQueue:             util.ParseString("RABBITMQ_TASKS_QUEUE", DefaultTasksQueue),
+		ResultsQueue:           util.ParseString("RABBITMQ_RESULTS_QUEUE", DefaultResultsQueue),
 	}, nil
 }
